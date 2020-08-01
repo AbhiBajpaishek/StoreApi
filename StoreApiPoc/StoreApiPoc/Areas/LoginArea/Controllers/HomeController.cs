16c20953eb3386bbb2658d61d8af9f9afe9ba1c6
AbhiBajpaishek <abhishekbajpai449@gmail.com>
2020-08-01 11:25:50 +0530

    UI for Login/Registration Done.

--- StoreApiPoc/StoreApiPoc/Areas/LoginArea/Controllers/HomeController.cs
+++ StoreApiPoc/StoreApiPoc/Areas/LoginArea/Controllers/HomeController.cs
@@ -9,14 +9,23 @@ namespace StoreApiPoc.Areas.LoginArea.Controllers
     public class HomeController : Controller
     {
         // GET: LoginArea/Home
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Registration()
         {
             return View();
         }
+
+        //[HttpPost]
+        //public ActionResult Registration()
+        //{
+        //    return View();
+        //}
+
     }
 }
